7b905e2d26eaff377b8ac002df69c12cc490c66f
CLouis98 <louis.carvalho98@gmail.com>
2025-09-03 16:22:53 +0200

    banned words txt ok

--- src/Service/Censurator.php
+++ src/Service/Censurator.php
@@ -4,7 +4,29 @@ namespace App\Service;
 
 class Censurator
 {
-    const BANNED_WORDS = ['pute', 'salope', 'connard', 'connasse', 'merde', 'putain', 'bordel', 'con', 'conne', 'bite', 'chatte', 'couille', 'foutre', 'baiser', 'niquer', 'enculé', 'enculée', 'gueule', 'crétin', 'imbécile', 'abruti', 'débile', 'taré', 'salaud', 'ordure', 'porc', 'chien', 'bâtard', 'salopard', 'fumier', 'racaille', 'cul', 'fesses', 'nichons', 'seins', 'zizi', 'pénis', 'vagin', 'sexe', 'cochon', 'dégueulasse', 'dégueu', 'caca', 'pipi', 'crotte', 'merdique'];
+    private array $bannedWords = [];
+
+    public function __construct(private string $bannedWordsFile)
+    {
+        $this->loadBannedWords();
+    }
+    private function loadBannedWords(): void
+    {
+        if (!file_exists($this->bannedWordsFile)) {
+            throw new \RuntimeException("Le fichier des mots bannis n'existe pas : {$this->bannedWordsFile}");
+        }
+
+        $content = file($this->bannedWordsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        if ($content === false) {
+            throw new \RuntimeException("Impossible de lire le fichier des mots bannis : {$this->bannedWordsFile}");
+        }
+
+        $this->bannedWords = array_map(function($word) {
+            return strtolower(trim($word));
+        }, $content);
+    }
+
     public function purify(string $string): string
     {
         $words = explode(' ', $string);
@@ -12,7 +34,7 @@ class Censurator
         foreach ($words as &$word) {
             $cleanWord = strtolower(trim($word, '.,!?;:'));
 
-            if (in_array($cleanWord, self::BANNED_WORDS)) {
+            if (in_array($cleanWord, $this->bannedWords)) {
                 $word = str_repeat('*', strlen($cleanWord));
             }
         }
